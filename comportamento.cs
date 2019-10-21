124773c882f7c2c0081ffa9c7542ac39ce2db820
pandora1431 <45410959+pandora1431@users.noreply.github.com>
2019-10-21 18:56:52 -0300

    Add files via upload

---
+++ comportamento.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comportamento : MonoBehaviour {
+
+	[SerializeField]
+	private float velocidade;
+	private Vector2 direcao;
+	
+	private Rigidbody2D rb2d;
+	
+	// Use this for initialization
+	void Start () {
+		
+		rb2d = GetComponent<Rigidbody2D>();
+		if (GameObject.Find("player").GetComponent<PlayerController>().viradoDireita == true)
+		{
+			direcao = Vector2.right;
+		}
+		if (GameObject.Find("player").GetComponent<PlayerController>().viradoDireita == false)
+		{
+			direcao = Vector2.left;
+			gameObject.GetComponent<SpriteRenderer>().flipX = true;
+		}
+	}
+	
+	void FixedUpdate () {
+	
+		rb2d.velocity = direcao * velocidade;
+		
+	}
+
+	void Update ()
+	{
+		Destroy(gameObject, 2f);
+	
+	}
+
+}
---
+++ tiro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tiro : MonoBehaviour {
+public Transform projetil;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown("x"))
+		{
+			Instantiate(projetil, this.gameObject.transform.GetChild(0).transform.position, Quaternion.identity);
+		}
+	}
+}

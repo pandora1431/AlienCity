fa63791fcac2f56d8d7f3ab140a4ab1b88fb48fc
pandora1431 <45410959+pandora1431@users.noreply.github.com>
2019-11-13 22:00:35 -0300

    Add files via upload

---
+++ armasel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class armasel : MonoBehaviour {
+
+	Text armahud;
+	PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		armahud = GetComponent<Text> ();
+		player = GameObject.Find("player").GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		armahud.text = "arma: " + player.armaAtual.ToString();
+		
+		/*
+		if (Input.GetKeyDown("z"))
+			{
+			if (arma == 1)
+				arma = 2
+			else
+				arma = 1
+			}
+		*/
+	}
+}
---
+++ tiro.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tiro : MonoBehaviour {
+public Transform projetil_1;
+public Transform projetil_2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+		
+	}
+
+    public void Atirar(int armaAtual)
+    {
+        if(armaAtual == 1)
+        {
+            Instantiate(projetil_1, this.gameObject.transform.GetChild(0).transform.position, Quaternion.identity);
+        }
+        else if (armaAtual == 2)
+        {
+            Instantiate(projetil_2, this.gameObject.transform.GetChild(0).transform.position, Quaternion.identity);
+        }
+            
+    }
+}

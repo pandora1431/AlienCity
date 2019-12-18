0d2a2748147d3b7f7aa069244a6c5bc8f15f2aa3
pandora1431 <45410959+pandora1431@users.noreply.github.com>
2019-12-18 09:26:33 -0300

    Add files via upload

---
+++ chavecoll.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class chavecoll : MonoBehaviour
+{
+    public Animator anim;
+    int countKey = 2;
+    public Text vitoria;
+    public float tempo = 0.0f;
+    public Text chaves;
+    private int key;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countKey = GameObject.FindGameObjectsWithTag("chave").Length;
+        key = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chaves.text = "key: " + key;
+        if (countKey == 0)
+        {
+            if(tempo == 0)
+            {
+                anim.SetTrigger("porta");
+            }
+            
+
+            tempo += Time.deltaTime;
+            if (tempo > 5f)
+            {
+                vitoria.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            }
+
+
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "chave")
+        {
+            key = key + 1;
+            countKey--;
+            Destroy(other.gameObject);
+        }
+        
+    }
+}
